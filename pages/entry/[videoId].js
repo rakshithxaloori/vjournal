5dc29ebf9e292565dd7eca921fb9a7ec6eecbf70
Rakshith Aloori <rakshithaloori10sa@gmail.com>
2023-06-17 22:11:26 +0530

    play vjournal, A-OKE

--- pages/entry/[videoId].js
+++ pages/entry/[videoId].js
@@ -1,4 +1,6 @@
+import { useRef, useEffect } from "react";
 import { getServerSession } from "next-auth/next";
+import dynamic from "next/dynamic";
 
 import { authOptions } from "pages/api/auth/[...nextauth]";
 
@@ -7,9 +9,68 @@ import Typography from "@mui/material/Typography";
 
 import { createServerAPIKit, networkError } from "@/utils/APIKit";
 
-const Journal = ({ video }) => {
-  console.log(video);
+const Entry = ({ video }) => {
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    const loadDash = async () => {
+      if (typeof window !== "undefined") {
+        const manifestUrl = await getManifestUrl();
+
+        const dashjs = await import("dashjs");
+        const player = dashjs.MediaPlayer().create();
+
+        // Initialize the player with new manifest
+        player.initialize(videoRef.current, manifestUrl, true);
+
+        player.on(dashjs.MediaPlayer.events.ERROR, (event) => {
+          console.error("Error occurred:", event.error);
+        });
+
+        // player.on(dashjs.MediaPlayer.events.MANIFEST_LOADED, (event) => {
+        //   console.log("Manifest loaded:", event);
+        // });
+
+        return () => {
+          player.reset();
+        };
+      }
+    };
+
+    loadDash();
+  }, []);
+
+  const getManifestUrl = async () => {
+    let manifestUrl = video.urls.mpd;
+    let videoUrl = video.urls.video;
+    let audioUrl = video.urls.audio;
+
+    const response = await fetch(manifestUrl);
+    const manifest = await response.text();
+
+    // Replace & with &amp;
+    videoUrl = videoUrl.replace(/&/g, "&amp;");
+    audioUrl = audioUrl.replace(/&/g, "&amp;");
+
+    // Replace the video and audio URLs with the ones we got from the server
+    const newManifest = manifest
+      .replace(
+        /<BaseURL>.*_video.mp4<\/BaseURL>/,
+        `<BaseURL>${videoUrl}</BaseURL>`
+      )
+      .replace(
+        /<BaseURL>.*_audio.mp4<\/BaseURL>/,
+        `<BaseURL>${audioUrl}</BaseURL>`
+      );
+
+    // Load new manifest into a new URL
+    const blob = new Blob([newManifest], { type: "application/dash+xml" });
+    manifestUrl = URL.createObjectURL(blob);
+    return manifestUrl;
+  };
+
   return (
+    // TODO head - {username}'s entry on {date}
     <Box>
       <Typography variant="h4" component="h4" color="primary">
         Journal
@@ -17,12 +78,15 @@ const Journal = ({ video }) => {
       <Typography variant="body1" color="primary">
         {video.title}
       </Typography>
-      <video src={video.url} controls></video>
+      <video ref={videoRef} autoPlay controls />;
     </Box>
   );
 };
 
-export default Journal;
+// export default Entry;
+export default dynamic(() => Promise.resolve(Entry), {
+  ssr: false,
+});
 
 export async function getServerSideProps(context) {
   const session = await getServerSession(context.req, context.res, authOptions);
