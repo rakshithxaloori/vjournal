dcf3043e5698b15443ff38b8fe48863ce567847d
Rakshith Aloori <rakshithaloori10sa@gmail.com>
2023-06-18 17:12:22 +0530

    fix player navigate bug

--- pages/entry/[videoId].js
+++ pages/entry/[videoId].js
@@ -10,80 +10,50 @@ import Typography from "@mui/material/Typography";
 import { createServerAPIKit, networkError } from "@/utils/APIKit";
 
 const Entry = ({ video }) => {
+  console.log(video);
   const videoRef = useRef(null);
+  const playerRef = useRef(null);
 
   useEffect(() => {
     const loadDash = async () => {
       if (typeof window !== "undefined") {
-        const manifestUrl = await getManifestUrl();
+        const manifestUrl = await getManifestUrl(video);
 
         const dashjs = await import("dashjs");
-        const player = dashjs.MediaPlayer().create();
+        playerRef.current = dashjs.MediaPlayer().create();
 
         // Initialize the player with new manifest
-        player.initialize(videoRef.current, manifestUrl, true);
-
-        player.on(dashjs.MediaPlayer.events.ERROR, (event) => {
-          console.error("Error occurred:", event.error);
-        });
-
-        // player.on(dashjs.MediaPlayer.events.MANIFEST_LOADED, (event) => {
-        //   console.log("Manifest loaded:", event);
-        // });
-
-        return () => {
-          player.reset();
-        };
+        playerRef.current.initialize(videoRef.current, manifestUrl, true);
       }
     };
 
     loadDash();
+    return () => {
+      if (playerRef.current) playerRef.current.reset();
+    };
   }, []);
-
-  const getManifestUrl = async () => {
-    let manifestUrl = video.urls.mpd;
-    let videoUrl = video.urls.video;
-    let audioUrl = video.urls.audio;
-
-    const response = await fetch(manifestUrl);
-    const manifest = await response.text();
-
-    // Replace & with &amp;
-    videoUrl = videoUrl.replace(/&/g, "&amp;");
-    audioUrl = audioUrl.replace(/&/g, "&amp;");
-
-    // Replace the video and audio URLs with the ones we got from the server
-    const newManifest = manifest
-      .replace(
-        /<BaseURL>.*_video.mp4<\/BaseURL>/,
-        `<BaseURL>${videoUrl}</BaseURL>`
-      )
-      .replace(
-        /<BaseURL>.*_audio.mp4<\/BaseURL>/,
-        `<BaseURL>${audioUrl}</BaseURL>`
-      );
-
-    // Load new manifest into a new URL
-    const blob = new Blob([newManifest], { type: "application/dash+xml" });
-    manifestUrl = URL.createObjectURL(blob);
-    return manifestUrl;
-  };
 
   return (
     // TODO head - {username}'s entry on {date}
-    <Box>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        height: "100%",
+        width: "100%",
+      }}
+    >
       <Typography variant="h4" component="h4" color="primary">
         Journal
       </Typography>
       <Typography variant="body1" color="primary">
         {video.title}
       </Typography>
-      <video ref={videoRef} autoPlay controls />;
+      <video ref={videoRef} autoPlay controls style={videoStyle} />
     </Box>
   );
 };
 
-// export default Entry;
 export default dynamic(() => Promise.resolve(Entry), {
   ssr: false,
 });
@@ -121,3 +91,41 @@ export async function getServerSideProps(context) {
       };
   }
 }
+
+const videoStyle = {
+  // height: "70vh",
+  // width: "auto",
+  width: "100%",
+  height: "auto",
+  aspectRatio: "16/9",
+  borderRadius: "20px",
+};
+
+const getManifestUrl = async (video) => {
+  let manifestUrl = video.urls.mpd;
+  let videoUrl = video.urls.video;
+  let audioUrl = video.urls.audio;
+
+  const response = await fetch(manifestUrl);
+  const manifest = await response.text();
+
+  // Replace & with &amp;
+  videoUrl = videoUrl.replace(/&/g, "&amp;");
+  audioUrl = audioUrl.replace(/&/g, "&amp;");
+
+  // Replace the video and audio URLs with the ones we got from the server
+  const newManifest = manifest
+    .replace(
+      /<BaseURL>.*_video.mp4<\/BaseURL>/,
+      `<BaseURL>${videoUrl}</BaseURL>`
+    )
+    .replace(
+      /<BaseURL>.*_audio.mp4<\/BaseURL>/,
+      `<BaseURL>${audioUrl}</BaseURL>`
+    );
+
+  // Load new manifest into a new URL
+  const blob = new Blob([newManifest], { type: "application/dash+xml" });
+  manifestUrl = URL.createObjectURL(blob);
+  return manifestUrl;
+};
