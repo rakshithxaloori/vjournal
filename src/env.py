8962219002fa2fb025e91f6a443ef7d5438b61b9
Rakshith Aloori <rakshithaloori10sa@gmail.com>
2023-06-21 21:56:17 +0530

    env changes

--- src/env.py
+++ src/env.py
@@ -4,7 +4,9 @@ from dotenv import load_dotenv
 load_dotenv()  # take environment variables from .env.
 
 
-AUDIO_GET_ENDPOINT = os.environ["AUDIO_GET_ENDPOINT"]
+BOSS_ENDPOINT = os.environ["BOSS_ENDPOINT"]
 BOSS_SECRET_KEY = os.environ["BOSS_SECRET_KEY"]
 OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
-SUBTITLES_PRESIGNED_GET_ENDPOINT = os.environ["SUBTITLES_PRESIGNED_GET_ENDPOINT"]
+
+AUDIO_GET_ENDPOINT = f"{BOSS_ENDPOINT}/audios/get/"
+SUBTITLES_PRESIGNED_GET_ENDPOINT = f"{BOSS_ENDPOINT}/subtitles/post/"
