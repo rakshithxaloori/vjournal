4d982af1640fee96b291eba2b1f651ed0c7539fa
Rakshith Aloori <rakshithaloori10sa@gmail.com>
2023-06-30 20:34:43 +0530

    use db for beta users

--- subscription/utils.py
+++ subscription/utils.py
@@ -2,13 +2,9 @@ from subscription.models import Customer
 from boss.models import BetaUser
 
 
-# BETA_USERS = ["rakshithaloori10sa@gmail.com", "shreyathokala60@gmail.com"]
-
-
 def get_subscription_info(user):
     customer = Customer.objects.filter(user=user).first()
     payload = {
-        # "is_beta": user.email in BETA_USERS,
         "is_beta": BetaUser.objects.filter(email=user.email).exists(),
         "cancel_at_period_end": None,
         "current_period_end": 0,
