55cb13cfa20f8865772a13578721feec90209074
Rakshith Aloori <rakshithaloori10sa@gmail.com>
2023-06-30 20:15:26 +0530

    beta user model init

--- boss/admin.py
+++ boss/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from boss.models import BetaUser
+
+admin.site.register(BetaUser)
--- boss/models.py
+++ boss/models.py
@@ -1,3 +1,13 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+
+class BetaUser(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email = models.EmailField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.email} | {self.created_at}"
