dea1c9920615d200563fb1d843390c1241c4ad91
Rakshith Aloori <rakshithaloori10sa@gmail.com>
2023-07-17 20:52:12 +0530

    add remove to sharelist A-OKE

--- components/modals/share.js
+++ components/modals/share.js
@@ -10,10 +10,38 @@ import { createClientAPIKit, networkError } from "@/utils/APIKit";
 
 const ShareModal = ({ open, setOpen, entry_id, setError }) => {
   const [loading, setLoading] = useState(false);
+  const [disabled, setDisabled] = useState(false);
   const [shareList, setShareList] = useState([]);
 
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
+
+  useEffect(() => {
+    const fetchShareList = async () => {
+      if (open) {
+        // Fetch the entry share list
+        setLoading(true);
+        try {
+          const APIKit = await createClientAPIKit();
+          const response = await APIKit.post("/api/share/list/", {
+            video_id: entry_id,
+          });
+          const { shares } = response.data?.payload;
+          setShareList(shares);
+          console.log("Share list", shares);
+        } catch (e) {
+          setError(networkError);
+        } finally {
+          setLoading(false);
+        }
+      } else {
+        // Reset the entry share list
+        setLoading(false);
+        setShareList([]);
+      }
+    };
+    fetchShareList();
+  }, [open]);
 
   const onChangeName = (e) => {
     setName(e.target.value);
@@ -45,34 +73,56 @@ const ShareModal = ({ open, setOpen, entry_id, setError }) => {
       setError("Invalid email.");
       return;
     }
+    // Check if duplicate
+    const emails = shareList.map((share) => share.email);
+    if (emails.includes(email)) {
+      setError("This email is already in the list.");
+      return;
+    }
+
+    // Add the share
+    setDisabled(true);
+    try {
+      setError("Adding to share list...");
+      const APIKit = await createClientAPIKit();
+      const response = await APIKit.post("/api/share/create/", {
+        video_id: entry_id,
+        contact_fullname: name,
+        contact_email: email,
+      });
+      if (response.status === 200) {
+        const { share } = response.data.payload;
+        setShareList([...shareList, share]);
+        setError("Added!");
+        setName("");
+        setEmail("");
+      }
+    } catch (e) {
+      setError(networkError(e));
+    } finally {
+      setDisabled(false);
+    }
   };
 
-  useEffect(() => {
-    const fetchShareList = async () => {
-      if (open) {
-        // Fetch the entry share list
-        setLoading(true);
-        try {
-          const APIKit = await createClientAPIKit();
-          const response = await APIKit.post("/api/share/list/", {
-            video_id: entry_id,
-          });
-          const { shares } = response.data?.payload;
-          setShareList(shares);
-          console.log("Share list", shares);
-        } catch (e) {
-          setError(networkError);
-        } finally {
-          setLoading(false);
-        }
-      } else {
-        // Reset the entry share list
-        setLoading(false);
-        setShareList([]);
+  const handleRemove = async (share_id) => {
+    setDisabled(true);
+    try {
+      setError("Removing from share list...");
+      const APIKit = await createClientAPIKit();
+      const response = await APIKit.post("/api/share/delete/", {
+        share_id,
+      });
+      if (response.status === 200) {
+        setShareList(shareList.filter((s) => s.id !== share_id));
+        setError("Removed!");
       }
-    };
-    fetchShareList();
-  }, [open]);
+    } catch (e) {
+      setError(networkError(e));
+    } finally {
+      setDisabled(false);
+    }
+  };
+
   return (
     <Modal open={open} onClose={() => setOpen(false)}>
       <Box sx={style}>
@@ -86,7 +136,7 @@ const ShareModal = ({ open, setOpen, entry_id, setError }) => {
           sx={{
             display: "flex",
             flexDirection: "row",
-            gap: 1,
+            gap: 2,
             my: 2,
           }}
         >
@@ -96,7 +146,7 @@ const ShareModal = ({ open, setOpen, entry_id, setError }) => {
             variant="outlined"
             value={name}
             onChange={onChangeName}
-            sx={{ width: "20%" }}
+            sx={{ flex: 2 }}
           />
           <TextField
             id="email"
@@ -104,23 +154,70 @@ const ShareModal = ({ open, setOpen, entry_id, setError }) => {
             variant="outlined"
             value={email}
             onChange={onChangeEmail}
-            sx={{ width: "60%" }}
+            sx={{ flex: 6 }}
           />
-          <Button
-            variant="contained"
-            sx={{ ml: 2, flexGrow: 1 }}
-            onClick={handleAdd}
-          >
+          <Button variant="contained" sx={{ flex: 2 }} onClick={handleAdd}>
             Add
           </Button>
         </Box>
 
-        <Box sx={{ display: "flex", flexDirection: "row", width: "100%" }}>
-          <Box sx={{ flexGrow: 1 }} />
+        {shareList.length > 0 ? (
+          <Box>
+            <Typography variant="h6" color="primary">
+              Shared with
+            </Typography>
+            {shareList.map((share) => (
+              // TODO use material ui list for scrollable. Also test it
+              <Box
+                key={share.id}
+                sx={{
+                  display: "flex",
+                  flexDirection: "row",
+                  alignItems: "center",
+                  gap: 2,
+                  my: 1,
+                }}
+              >
+                <Typography
+                  variant="body1"
+                  color="textSecondary"
+                  sx={{ flex: 2 }}
+                >
+                  {share.contact.name}
+                </Typography>
+                <Typography
+                  variant="body1"
+                  color="textSecondary"
+                  sx={{ flex: 6 }}
+                >
+                  {share.contact.email}
+                </Typography>
+                <Button
+                  variant="contained"
+                  sx={{ flex: 2 }}
+                  onClick={() => handleRemove(share.id)}
+                  disabled={disabled}
+                >
+                  Remove
+                </Button>
+              </Box>
+            ))}
+          </Box>
+        ) : (
+          <Typography variant="body2" color="textSecondary">
+            No one else has access to this entry yet.
+          </Typography>
+        )}
+
+        <Box
+          sx={{ display: "flex", flexDirection: "row", width: "100%", gap: 2 }}
+        >
+          <Box sx={{ flex: 9 }} />
           <Button
-            sx={{ mt: 2 }}
+            sx={{ flex: 1 }}
             variant="contained"
             onClick={() => setOpen(false)}
+            disabled={disabled}
           >
             Done
           </Button>
